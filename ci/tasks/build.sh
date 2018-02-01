fe8d83a84e9b5ff50efc714026d5e9073c1e3a77
Domas Juodele <domas.juodele@gmail.com>
2018-02-02 00:08:06 +0100

    works

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -3,59 +3,13 @@
 set -e -u -x
 
 npm -v
-
 ng -v
-
-ls -la
-
-cd ..
-
-ls -la
-
-cd -
-
-ls -la
 
 cd ng-concource-ci
 
-pwd
-
-ls -la
-
 npm install
-
 ng build
 
 cp Staticfile root
-ls -la
-
-cd ..
-ls -la
-
-cd build-out
-ls -la
-
-pwd
-cd ..
-cd ng-concource-ci/root
-ls -la
-cd ..
-
 cp -r . ../build-out
-
-cd ..
-
-cd build-out
-ls -la
-
-pwd
-
-cd ..
-
-pwd
-ls -la
-
-
-
-echo "done 4"
 
