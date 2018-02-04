544f1e70b2cf97f74c353c2e26c0700e05c5ddf1
Domas Juodele <domas.juodele@gmail.com>
2018-02-04 21:52:56 +0100

    x

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -4,13 +4,23 @@ set -e -u -x
 
 cd ng-concource-ci
 
-#npm install
-
-#ng build
-
-#cp Staticfile root
-#cp -r . ../build-out
+npm install
+ng build
 
 
-cp -r ./manifest.yml ../build-out/manifest-5.yml
+
+cp Staticfile root
+mkdir artifact
+cp -r root artifact
+cp manifest.yml artifact
+
+tar cvzf artifact-1.tar.gz artifact/
+
+
+cp -r artifact-1.tar.gz ../build-out
+
+
+
+
+#cp -r ./manifest.yml ../build-out/manifest-5.yml
 
---
+++ ci/tasks/release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e -u -x
+
+
+pwd
+
+ls -la
+
+cd storage
+
+#npm install
+
+#ng build
+
+#cp Staticfile root
+#cp -r . ../build-out
+
+
+cp -r ./manifest.yml ../build-out/manifest-5.yml
+
